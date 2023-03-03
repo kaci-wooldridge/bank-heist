fa8dcae55e50ec5d007d3701907262b88a5f7450
kaci <kaci.school.info@gmail.com>
2023-03-03 10:10:28 -0600

    done maybe

--- Program.cs
+++ Program.cs
@@ -9,12 +9,17 @@ namespace BankHeist
         {
             Dictionary<string, TeamMember> TheSquad = new();
 
+            Console.WriteLine("Plan Your Heist");
+            Console.WriteLine("_ _ _ _ _ _ _ _ _ _");
+            Console.WriteLine("");
+
+            Console.WriteLine("Please choose a difficulty level for your mission. (1-100)");
+            string difficultyLevel = Console.ReadLine();
+            int bankDifficulty = int.Parse(difficultyLevel);
+            Console.WriteLine("");
+
             while (true)
             {
-                Console.WriteLine("Plan Your Heist");
-                Console.WriteLine("_ _ _ _ _ _ _ _ _ _");
-                Console.WriteLine("");
-
                 Console.WriteLine("What is your team member's name");
                 TeamMember TeamMember = new();
                 string givenName = Console.ReadLine();
@@ -57,12 +62,19 @@ namespace BankHeist
 
             }
 
-            Console.WriteLine("How many times would you like to run this simulation? (1-5)");
+            Console.WriteLine("How many times would you like to run this simulation? (1-10)");
             string trialRuns = Console.ReadLine();
             int trialRunsInt = int.Parse(trialRuns);
+            Console.Clear();
 
-            int bankDifficulty = 100;
             int teamSkillLevel = 0;
+            int failedRuns = 0;
+            int successRuns = 0;
+
+            foreach (KeyValuePair<string, TeamMember> member in TheSquad)
+            {
+                teamSkillLevel += member.Value.SkillLevel;
+            }
 
             for (int i = 0; i < trialRunsInt; i++)
             {
@@ -71,22 +83,28 @@ namespace BankHeist
 
                 bankDifficulty += luckValue;
 
-                foreach (KeyValuePair<string, TeamMember> member in TheSquad)
-                {
-                    teamSkillLevel += member.Value.SkillLevel;
-                }
-
                 Console.WriteLine($"You team's skill level is {teamSkillLevel}. The bank's difficulty level is {bankDifficulty}.");
 
                 if (teamSkillLevel >= bankDifficulty)
                 {
                     Console.WriteLine("You robbed the crap out of that bank");
+                    Console.WriteLine("_ _ _ _ _ _ _ _ _ _ _ _");
+                    Console.WriteLine("");
+                    successRuns += 1;
                 }
                 else
                 {
                     Console.WriteLine("Wow you got caught so fast");
+                    Console.WriteLine("_ _ _ _ _ _ _ _ _ _ _ _");
+                    Console.WriteLine("");
+                    failedRuns += 1;
+
                 }
+
+                bankDifficulty = int.Parse(difficultyLevel);
             }
+
+            Console.WriteLine($"Your team successfully robbed {successRuns} banks. Your team  failed and got arrested {failedRuns} times.");
 
         }
     }
